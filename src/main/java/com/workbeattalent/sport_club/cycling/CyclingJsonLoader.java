318ddca8d69207152bcaf6554fa01c9b2bcc53ce
leonel ka <waboleonel@gmail.com>
2024-09-25 10:21:42 +0100

    Add condition to avoid loading data from json data file at each application reload

--- src/main/java/com/workbeattalent/sport_club/cycling/CyclingJsonLoader.java
+++ src/main/java/com/workbeattalent/sport_club/cycling/CyclingJsonLoader.java
@@ -2,6 +2,8 @@ package com.workbeattalent.sport_club.cycling;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 @Component
 public class CyclingJsonLoader implements CommandLineRunner {
+
+    private final static Logger LOG = LoggerFactory.getLogger(CyclingJsonLoader.class);
 
     private final CyclingService cyclingService;
     private final ObjectMapper objectMapper;
@@ -20,11 +24,15 @@ public class CyclingJsonLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        try (final var data = TypeReference.class.getResourceAsStream("/data/cycling_tours.json")) {
-            final var cyclingTours = this.objectMapper.readValue(data, CyclingTourList.class);
-            this.cyclingService.saveAll(cyclingTours.tours());
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to read the data file");
+        if (this.cyclingService.count() == 0) {
+            try (final var data = TypeReference.class.getResourceAsStream("/data/cycling_tours.json")) {
+                final var cyclingTours = this.objectMapper.readValue(data, CyclingTourList.class);
+                this.cyclingService.saveAll(cyclingTours.tours());
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to read the data file");
+            }
+        } else {
+            LOG.info("Not loading data from JSON file because the collection contains some data!");
         }
     }
 }
