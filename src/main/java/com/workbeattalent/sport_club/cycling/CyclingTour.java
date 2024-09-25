d98e97a6cc200687acce7aa3f8ec9dc564a40045
leonel ka <waboleonel@gmail.com>
2024-09-25 10:10:04 +0100

    Complain to Data JDBC entity definition

--- src/main/java/com/workbeattalent/sport_club/cycling/CyclingTour.java
+++ src/main/java/com/workbeattalent/sport_club/cycling/CyclingTour.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
@@ -12,12 +14,15 @@ import java.time.LocalDateTime;
 @ToString
 @NoArgsConstructor
 public class CyclingTour {
+    @Id
     private Long id;
     private String title;
     private LocalDateTime startedOn;
     private LocalDateTime completedOn;
     private Double distance;
     private Location location;
+    @Version
+    private Integer version;
 
     public CyclingTour(String title, LocalDateTime startedOn, LocalDateTime completedOn, Double distance, Location location) {
         this.title = title;
