1475e70d21e2b3b5d5d894d6132f53cbf8a158dd
leonel ka <waboleonel@gmail.com>
2024-09-25 21:10:48 +0100

    Make distance float type

--- src/main/java/com/workbeattalent/sport_club/cycling/CyclingTour.java
+++ src/main/java/com/workbeattalent/sport_club/cycling/CyclingTour.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
@@ -13,22 +14,32 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @NoArgsConstructor
+@Table(name = "cycling") // Because Data JDBC assumes the table name is 'cycling_tour' as class name
 public class CyclingTour {
     @Id
     private Long id;
     private String title;
     private LocalDateTime startedOn;
     private LocalDateTime completedOn;
-    private Double distance;
+    private Float distance;
     private Location location;
     @Version
     private Integer version;
 
-    public CyclingTour(String title, LocalDateTime startedOn, LocalDateTime completedOn, Double distance, Location location) {
+    public CyclingTour(String title, LocalDateTime startedOn, LocalDateTime completedOn, Float distance, Location location) {
         this.title = title;
         this.startedOn = startedOn;
         this.completedOn = completedOn;
         this.distance = distance;
         this.location = location;
+    }
+
+    public CyclingTour(String title, LocalDateTime startedOn, LocalDateTime completedOn, Float distance, Location location, Integer version) {
+        this.title = title;
+        this.startedOn = startedOn;
+        this.completedOn = completedOn;
+        this.distance = distance;
+        this.location = location;
+        this.version = version;
     }
 }
