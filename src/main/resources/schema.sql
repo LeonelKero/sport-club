011655bba8b9eeff35e46fc3ccf3a1b3b176a0cc
leonel ka <waboleonel@gmail.com>
2024-09-25 09:52:40 +0100

    Update schema.sql for PostgreSQL technology

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,18 @@
+-- Create database if not exists
+SELECT 'CREATE DATABASE sports'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'sports');
+
+-- Define sequence
+CREATE SEQUENCE IF NOT EXISTS cycling_id_seq INCREMENT BY 1 MINVALUE 1;
+-- Create cycling table with that sequence
 create table if not exists cycling (
-id int auto_increment not null,
+id int NOT NULL DEFAULT nextval('cycling_id_seq'),
 title varchar(250) not null,
 started_on timestamp not null,
 completed_on timestamp not null,
-distance double not null,
+distance double precision not null,
 location varchar(20) not null,
 primary key (id)
 );
+-- Alter the sequence
+ALTER SEQUENCE cycling_id_seq OWNED BY cycling.id;
