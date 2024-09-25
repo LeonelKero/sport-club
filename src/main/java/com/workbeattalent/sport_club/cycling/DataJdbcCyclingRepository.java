4c4b9025885951e9e39fa5b5261cc78905062c91
leonel ka <waboleonel@gmail.com>
2024-09-25 21:20:28 +0100

    Add custom function query

--- src/main/java/com/workbeattalent/sport_club/cycling/DataJdbcCyclingRepository.java
+++ src/main/java/com/workbeattalent/sport_club/cycling/DataJdbcCyclingRepository.java
@@ -2,5 +2,8 @@ package com.workbeattalent.sport_club.cycling;
 
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface DataJdbcCyclingRepository extends ListCrudRepository<CyclingTour, Long> {
+    List<CyclingTour> findAllByLocation(final String location);
 }
