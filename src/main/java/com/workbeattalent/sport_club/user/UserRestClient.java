49117f22929c8744e18266500df551f084694dce
leonel ka <waboleonel@gmail.com>
2024-09-25 21:44:09 +0100

    Implement user rest client

---
+++ src/main/java/com/workbeattalent/sport_club/user/UserRestClient.java
@@ -0,0 +1,34 @@
+package com.workbeattalent.sport_club.user;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.Collection;
+
+@Component
+public class UserRestClient {
+
+    private final RestClient restClient;
+
+    public UserRestClient(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .build();
+    }
+
+    public Collection<User> getAllUsers() {
+        return this.restClient.get()
+                .uri("/users")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+    }
+
+    public User getById(final Long id) {
+        return this.restClient.get()
+                .uri("/users/{id}", id)
+                .retrieve()
+                .body(User.class);
+    }
+}
